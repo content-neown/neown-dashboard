8c1eb8836cd37afad91c24ffcd85f4bc96ede030
content-neown <content.neown@gmail.com>
2026-06-11 12:05:30 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -263,7 +263,7 @@ function ST({children}){return<div style={{fontSize:11,fontWeight:500,color:'#aa
 
 /* ── Main page ── */
 export default function Dashboard(){
-  const[url,setUrl]=useState(''),[oSheet,setOSheet]=useState('Sheet1'),[pSheet,setPSheet]=useState('Sheet2');
+  const[url,setUrl]=useState(''),[oSheet,setOSheet]=useState('Orders'),[pSheet,setPSheet]=useState('India Post Pincode');
   const[status,setStatus]=useState({msg:'',type:''}),[appData,setAppData]=useState(null);
   const[filter,setFilter]=useState('all'),[tab,setTab]=useState('charts'),[mapMet,setMapMet]=useState('orders');
   const cRef=useRef({});
